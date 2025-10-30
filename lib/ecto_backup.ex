19d6f05ed48170b807f5b1a17de54adfa5569a05
Nathan Shafer <nate@lotech.org>
2025-10-30 15:07:00 -0700

    Initial checkin

---
+++ lib/ecto_backup.ex
@@ -0,0 +1,18 @@
+defmodule EctoBackup do
+  @moduledoc """
+  Documentation for `EctoBackup`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EctoBackup.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ecto_backup_test.exs
@@ -0,0 +1,8 @@
+defmodule EctoBackupTest do
+  use ExUnit.Case
+  doctest EctoBackup
+
+  test "greets the world" do
+    assert EctoBackup.hello() == :world
+  end
+end
